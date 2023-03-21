621097f2616d5498153e4b2116cff3add7734cf7
郭江龙 <baozi_glb@qq.com>
2023-03-21 14:50:35 +0800

    112version 221version

--- src/main/java/com/example2/demo2/Demo2Application.java
+++ src/main/java/com/example2/demo2/Demo2Application.java
@@ -8,6 +8,7 @@ public class Demo2Application {
 
     public static void main(String[] args) {
         System.out.println(112);
+        System.out.println(221);
         SpringApplication.run(Demo2Application.class, args);
     }
 
